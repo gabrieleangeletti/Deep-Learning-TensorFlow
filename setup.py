b51e8af043e0fbab40d66f8c2a8982395679d562
blackecho <angeletti.gabriele@gmail.com>
2016-04-20 11:31:28 +0200

    Changed project structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'pydeep',
+    packages = ['pydeep'],
+    version = '0.1',
+    description = 'Train Deep Learning models from the command line.',
+    author = 'Gabriele Angeletti',
+    author_email = 'angeletti.gabriele@gmail.com',
+    url = 'https://github.com/blackecho/Deep-Learning-TensorFlow',
+    download_url = 'https://github.com/blackecho/Deep-Learning-TensorFlow/tarball/0.1',
+    keywords = ['deep-learning', 'neural-networks', 'tensorflow'],
+    classifiers = [],
+)
