15a0d5ab41a71c4587bb19ebc26f0afa9cff5ab0
blackecho <angeletti.gabriele@gmail.com>
2016-05-06 13:06:35 +0200

    minor

--- model.py
+++ model.py
@@ -98,9 +98,8 @@ class Model(object):
 
         with tf.name_scope("cost"):
             if loss_func == 'cross_entropy':
-                # cost = - tf.reduce_mean(ref_input * tf.log(model_output) +
-                #                              (1 - ref_input) * tf.log(1 - model_output))
-                cost = tf.reduce_mean(-tf.reduce_sum(ref_input * tf.log(model_output), reduction_indices=[1]))
+                cost = - tf.reduce_mean(ref_input * tf.log(model_output) +
+                                             (1 - ref_input) * tf.log(1 - model_output))
                 _ = tf.scalar_summary("cross_entropy", cost)
                 self.cost = cost
 
