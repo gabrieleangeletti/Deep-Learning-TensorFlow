ce9e9ded3af911e3d73182c24e992293ceb5e52a
Ubuntu <ubuntu@ip-172-31-21-182.us-west-2.compute.internal>
2017-01-14 14:04:27 +0000

    edit config

--- command_line/config.py
+++ command_line/config.py
@@ -1,3 +1,3 @@
-models_dir = 'stored_models/'  # dir to save/restore stored_models
-data_dir = 'data/'  # directory to store algorithm data
-summary_dir = 'logs/'  # directory to store tensorflow summaries
+models_dir = 'home/ubuntu/pynb/model/stored_models/'  # dir to save/restore stored_models
+data_dir = 'home/ubuntu/pynb/model/data/'  # directory to store algorithm data
+summary_dir = 'home/ubuntu/pynb/model/logs/'  # directory to store tensorflow summaries
