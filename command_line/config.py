f2ec73a2789acad652d31d900d583587879c5d0e
Gabriele Angeletti <angeletti.gabriele@gmail.com>
2017-02-08 23:41:57 +0100

    Minor.

--- command_line/config.py
+++ command_line/config.py
@@ -1,6 +1,4 @@
-models_dir = 'home/ubuntu/pynb/model/stored_models/'  # dir to save/restore stored_models
-data_dir = 'home/ubuntu/pynb/model/data/'  # directory to store algorithm data
-summary_dir = 'home/ubuntu/pynb/model/logs/'  # directory to store tensorflow summaries
-
-vocab_dir  = '/home/ubuntu/pynb/Data/flickr_data/text/vocab.txt' # directory of the 2000 most common tags. 
+models_dir = 'stored_models/'  # dir to save/restore stored_models
+data_dir = 'data/'  # directory to store algorithm data
+summary_dir = 'logs/'  # directory to store tensorflow summaries
 
