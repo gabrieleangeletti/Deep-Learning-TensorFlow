5086dd5f44539c8da7a3c4a3fdfe905d94f51142
Gabriele Angeletti <angeletti.gabriele@gmail.com>
2017-02-10 19:50:23 +0100

    Version 0.0.5.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='yadlt',
-    version='0.0.4',
+    version='0.0.5',
     url='https://github.com/blackecho/Deep-Learning-Tensorflow',
-    download_url='https://github.com/blackecho/Deep-Learning-TensorFlow/tarball/0.0.2rc3',
+    download_url='https://github.com/blackecho/Deep-Learning-TensorFlow/tarball/0.0.5rc3',
     author='Gabriele Angeletti',
     author_email='angeletti.gabriele@gmail.com',
     description='Implementation of various deep learning algorithms using Tensorflow. Class interfaces is sklearn-like.',
-    packages=find_packages(exclude=['tests', 'data', 'logs', 'stored_models']),
+    packages=find_packages(exclude=['tests']),
     zip_safe=False,
     include_package_data=True,
     platforms='any',
